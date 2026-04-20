19d127a4d2c3621f468e8520d93301dbe3ee91ae
Dautkhan <bbdavi@icloud.com>
2026-04-20 18:52:53 +0500

    Add shop urls

--- backend/shop/urls.py
+++ backend/shop/urls.py
@@ -1,7 +1,14 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from .views import(
+    category_list,
+    product_list,
+    CategoryDetailAPIView,
+    ProductDetailAPIView,
+)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('shop.urls')),
+    path('categories/',category_list, name = 'category_list'),
+    path('products/',product_list, name = 'product_list'),
+    path('categories/<int:pk>/',CategoryDetailAPIView.as_view(), name = 'category_detail'),
+    path('products/<int:pk>/', ProductDetailAPIView.as_view(), name = 'product_detail'),
 ]
